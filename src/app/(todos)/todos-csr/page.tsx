dd9c5671843a91a36a1117c07294c416fa52feec
baram55 <asd7973@gmail.com>
2024-03-13 19:20:12 +0900

    Feat : react-query 설치 및 설정

---
+++ src/app/(todos)/todos-csr/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodosCsr = () => {
+  return <div>TodosCsr</div>;
+};
+
+export default TodosCsr;
---
+++ src/app/(todos)/todos-ssr/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodosSsr = () => {
+  return <div>TodosSsr</div>;
+};
+
+export default TodosSsr;
