cd64fe36fe6fac180430a0a579b8351bf19ffe87
baram55 <asd7973@gmail.com>
2024-03-14 12:25:49 +0900

    Feat : todos 라우트 핸들러 수정

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -4,5 +4,24 @@ export const GET = async () => {
   });
   const todos = await response.json();
 
+  if (!todos) {
+    return new Response("todos not found", { status: 404 });
+  }
+
   return Response.json(todos);
+};
+
+export const POST = async (request: Request) => {
+  const { title, contents } = await request.json();
+
+  const response = await fetch(`http://localshost:4000/todos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, contents, isDone: false }),
+  });
+  const todo = await response.json();
+
+  return Response.json({ todo });
 };
