5b82aec798cf2e20f50079850c13543bf9cc8935
baram55 <asd7973@gmail.com>
2024-03-16 21:54:36 +0900

    Feat : 코드 리셋

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import QueryProvider from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,23 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>
-        <nav>
-          <Link className="mx-4" href="/about">
-            About
-          </Link>
-          <Link href="/report" className="mx-4">
-            Report
-          </Link>
-          <Link href="/todos-csr" className="mx-4">
-            Todos-csr
-          </Link>
-          <Link href="/todos-ssr" className="mx-4">
-            Todos-ssr
-          </Link>
-        </nav>
-        <QueryProvider>{children}</QueryProvider>
-      </body>
+      <body className={inter.className}></body>
     </html>
   );
 }
